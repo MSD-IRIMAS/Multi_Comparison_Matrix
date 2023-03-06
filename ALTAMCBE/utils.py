6f37af366acebf597ab7cfdace2f734e1913385d
hadifawaz1999 <hadifawaz2291999@gmail.com>
2023-03-06 15:42:46 +0100

    added docstrings for utils.py functions

--- ALTAMCBE/utils.py
+++ ALTAMCBE/utils.py
@@ -8,36 +8,82 @@ from scipy.stats import wilcoxon
 from baycomp import SignedRankTest
 
 def decode_results_data_frame(df, analysis):
+    """
+    
+    Decode the necessary information from the data frame and put them into the json analysis file.
 
-    df_columns = list(df.columns)
+    Parameters
+    ----------
 
+    df : pandas DataFrame containing the statistics of each classifier on multiple datasets.
+         shape = (n_datasets, n_classifiers), columns = [list of classifiers names]
+    analysis : python dictionary
+
+    """
+
+    df_columns = list(df.columns) # extract columns from data frame
+
+    # check if dataset column name is correct
     if analysis['dataset-column'] not in df_columns:
         raise KeyError("The column "+analysis['dataset-column']+" is missing.")
 
+    # get number of examples (datasets)
     n_datasets = len(np.unique(np.asarray(df[analysis['dataset-column']])))
 
-    analysis['n-datasets'] = n_datasets
+    analysis['n-datasets'] = n_datasets # add number of examples to dictionary
 
-    analysis['dataset-names'] = list(df[analysis['dataset-column']])
+    analysis['dataset-names'] = list(df[analysis['dataset-column']]) # add example names to dict
     
-    df_columns.remove(analysis['dataset-column'])
+    df_columns.remove(analysis['dataset-column']) # drop the dataset column name from columns list
+    # and keep classifier names
 
     classifier_names = df_columns.copy()
     n_classifiers = len(classifier_names)
 
+    # add the information about classifiers to dict
     analysis['classifier-names'] = classifier_names
     analysis['n-classifiers'] = n_classifiers
 
 def get_pairwise_content(x,
                          y,
                          order_WinTieLoss='higher',
-                         includeProbaWinTieLoss=True,
+                         includeProbaWinTieLoss=False,
                          include_pvalue=True,
                          pvalue_test='wilcoxon',
                          pvalue_correction=None,
                          pvalue_threshhold=0.05,
                          used_mean='arithmetic-mean',
                          bayesian_rope=0.01):
+
+    """
+    
+    Get the pairwise comparison between two classifiers on all the given datasets.
+
+    Parameters:
+    -----------
+
+    x : ndarray of shape = (n_datasets,) containing the statistics of a classifier x
+        on all the datasets
+    y : ndarray of shape = (n_datasets,) containing the statistics of a classifier y
+        on all the datasets
+    
+    order_WinTieLoss : str, default = 'higher', the order on considering a win or a loss
+                       for a given statistics
+    includeProbaWinTieLoss : bool, default = False, condition whether or not include
+                             the bayesian test of [1] for a probabilistic win tie loss count
+    include_pvalue : bool, default = True, condition whether or not include a pvalue stats
+    pvalue_test : str, default = 'wilcoxon', the statistical test to produce the pvalue stats.
+    pvalue_correction : str, default = None, which correction to use for the pvalue significant test
+    pvalue_threshhold : float, default = 0.05, threshold for considering a comparison is significant
+                        or not. If pvalue < pvalue_threshhold -> comparison is significant.
+    used_mean : str, default = 'arithmetic', the mean used to comapre two classifiers.
+    bayesian_rope : float, default = 0.01, the rope used in case include_ProbaWinTieLoss is True
+
+    Returns
+    -------
+    content : python dictionary
+
+    """
 
     content = {}
 
@@ -88,6 +134,19 @@ def get_pairwise_content(x,
     return content
 
 def re_order_classifiers(df_results, analysis):
+
+    """
+    
+    Re order classifiers given a specific order stats.
+
+    Parameters
+    ----------
+
+    df_results : pandas DataFrame containing the results of each classifier for all datasets
+    analysis : python dictionary containing the information of the pairwise comparison,
+               the ordering information will be added to this dictionary
+    
+    """
 
     stats = []
 
@@ -150,7 +209,23 @@ def re_order_classifiers(df_results, analysis):
     analysis['ordered-stats'] = list(np.asarray(stats)[ordered_indices])
     analysis['ordered-classifier-names'] = list(np.asarray(analysis['classifier-names'])[ordered_indices])
 
-def get_ticks(analysis, round_number):
+def get_ticks(analysis):
+
+    """
+    
+    Generating tick labels for the heatmap.
+
+    Parameters
+    ----------
+
+    analysis : python dictionary containing all the information about the classifiers and comparisons
+
+    Returns
+    -------
+
+    ticks : list of str, containing the tick labels for each classifer
+    
+    """
 
     ticks = []
 
@@ -160,11 +235,29 @@ def get_ticks(analysis, round_number):
         ordering = analysis['order-stats']
 
     for i in range(analysis['n-classifiers']):
-        ticks.append(analysis['ordered-classifier-names'][i] + '\n' + ordering + '\n' + str(round(analysis['ordered-stats'][i], round_number)))
+        ticks.append(analysis['ordered-classifier-names'][i] + '\n' + ordering + '\n' + str(round(analysis['ordered-stats'][i], 4)))
 
     return ticks
 
-def get_ticks_heatline(analysis, proposed_method, round_number):
+def get_ticks_heatline(analysis, proposed_method):
+
+    """
+    
+    Generating tick labels for the heat line.
+
+    Parameters
+    ----------
+
+    analysis : python dictionary containing all the information about the classifiers and comparisons
+    proposed_method : str, name of the classifier that you want to compare with all the others
+    
+    Returns
+    -------
+
+    xticks : list of str, containing the tick labels for each classifer
+    yticks : list of only one str, containing one tick of the classifier in question (proposed_method)
+    
+    """
 
     xticks = []
     yticks = []
@@ -174,19 +267,49 @@ def get_ticks_heatline(analysis, proposed_method, round_number):
         if analysis['ordered-classifier-names'][i] == proposed_method:
             yticks.append(proposed_method + ' VS')
         
-        xticks.append(analysis['ordered-classifier-names'][i] + '\n' + analysis['order-stats'] + '\n' + str(round(analysis['ordered-stats'][i], round_number)))
+        xticks.append(analysis['ordered-classifier-names'][i] + '\n' + analysis['order-stats'] + '\n' + str(round(analysis['ordered-stats'][i], 4)))
         
     return xticks, yticks
 
 def abs(x):
 
+    """
+    
+    Absolute value.
+    
+    """
+
     return x if x > 0 else -x
 
 def get_sign(x):
 
+    """
+    
+    Sign of a value
+
+    """
+
     return 1 if x > 0 else -1
 
 def get_limits(pairwise_matrix):
+
+    """
+    
+    Get the limits, min and max, of the heatmap color bar values
+    min and max produced are equal in absolute value, to insure symmetry
+
+    Parameters
+    ----------
+    pairwise_matrix : ndarray, shape = (n_classifiers, n_classifiers), a matrix containing 
+                      the 1v1 statistical values (by default: difference of arithmetic mean of stats)
+    
+    Returns
+    -------
+    
+    min_value : float, the min value
+    max_value : float, the max value
+    
+    """
 
     if pairwise_matrix.shape[0] == 1:
 
@@ -224,6 +347,12 @@ def get_fig_size(fig_size,
                  pixels_per_clf_hieght,
                  pixels_per_clf_width,
                  n_info_per_line=None):
+    
+    """
+    
+    Generate figure size given the input parameters
+    
+    """
 
     if fig_size == 'auto':
 
@@ -248,6 +377,30 @@ def plot_1v1(x,
              loss_x,
              tie,
              output_directory='./'):
+    
+    """
+    
+    Plot the 1v1 scatter values of two classifiers x and y on all the datasets
+    Inlude the win tie loss count and the pvalue stats
+
+    Parameters:
+    -----------
+
+    x : ndarray, shape = (n_datasets,), containing statistics of a classifier x on all datasets
+    y : ndarray, shape = (n_datasets,), containing statistics of a classifier y on all datasets
+    name_x : str, name of classifier x
+    name_y : str, name of classifier y
+    win_x : number of wins for x
+    tie : number of ties
+    loss_x : number of loss for y
+    output_directory : str, default = './', output directory of the 1v1 plots
+
+    Plot
+    ----
+    On the y-axis, the classifier y values will be scattered, and the x values on the x-axis
+    The points in blue are wins for y, points in orange are wins for x
+    
+    """
 
     if os.path.exists(output_directory+'1v1_plots/'+name_x+'_vs_'+name_y+'.pdf'):
         return
