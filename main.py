2079d4b852f7c708992fe228aec9015529a1492b
hadifawaz1999 <hadifawaz2291999@gmail.com>
2023-03-14 10:45:11 +0100

    changed name to MCM

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from ALTAMCBE import ALTAMCBE
+from MCM import MCM
 
 if __name__ == "__main__":
 
     path_res = './results_example.csv'
     output_dir = './'
 
-    analysis = ALTAMCBE.get_analysis(df_results_path=path_res,
+    analysis = MCM.get_analysis(df_results_path=path_res,
                                      save_as_json=True,
                                      plot_1v1_comparisons=True,
                                      output_dir=output_dir)
     
-    ALTAMCBE.get_heatmap(output_dir=output_dir,
+    MCM.get_heatmap(output_dir=output_dir,
                          colormap='coolwarm',
                          show_symetry=True)
 
-    ALTAMCBE.get_line_heatmap(proposed_method='clf3', output_dir=output_dir, colormap='coolwarm',pixels_per_clf_hieght=7)
+    MCM.get_line_heatmap(proposed_method='clf3', output_dir=output_dir, colormap='coolwarm',pixels_per_clf_hieght=7)
