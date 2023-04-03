d32ff04226bc7b673d7299eacfbade03f4863949
hadifawaz1999 <hadifawaz2291999@gmail.com>
2023-04-03 15:01:05 +0200

    [DOC] changed example

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ from MCM import MCM
 
 if __name__ == "__main__":
 
-    path_res = './results_ucr2.csv'
+    path_res = './results_example_no_data_column2.csv'
     output_dir = './'
 
     df_results = pd.read_csv(path_res)
 
     MCM.compare(
         df_results=df_results,
-        show_symetry=False,
-        excluded_row_comparates=['ROCKET']
+        fig_savename='heatmap',
+        load_analysis=False
+    )
+
+    MCM.compare(
+        df_results=df_results,
+        excluded_col_comparates=['clf1','clf3'],
+        fig_savename='heatline_vertical',
+        load_analysis=False
+    )
+
+    MCM.compare(
+        df_results=df_results,
+        excluded_row_comparates=['clf1','clf3'],
+        fig_savename='heatline_horizontal',
+        load_analysis=False
     )
